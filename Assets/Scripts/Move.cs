7097050a6b431c0ddb059c20f730cc42043c05c1
kieuminhdat <datkmhe150913@fpt.edu.vn>
2023-06-19 02:28:50 +0700

    Player movement done

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private VariableJoystick joystick;
+
+    private float moveDistance = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalMove = Input.GetAxis("Horizontal");
+        var verticalMove = Input.GetAxis("Vertical");
+
+        horizontalMove = joystick.Horizontal;
+        verticalMove = joystick.Vertical;
+
+        float moveX = 0;
+
+        if (horizontalMove != 0)
+        {
+            moveX = horizontalMove < 0 ? -moveDistance : moveDistance;
+        }
+
+        float moveY = 0;
+
+        if (verticalMove != 0)
+        {
+            moveY = verticalMove < 0 ? -moveDistance : moveDistance;
+        }
+
+        if (moveX != 0 || moveY != 0)
+        {
+            var pos = playerTransform.position;
+            playerTransform.position = new Vector3(pos.x + moveX, pos.y + moveY, pos.z);
+        }
+    }
+}

75b93aa23eb2a4789234078017a08f89c1be8874
Nguyen Phong Hao <haonphe153346@fpt.edu.vn>
2023-06-20 11:05:29 +0700

    Hasagi_Skill

---
+++ Assets/Scripts/SKills/SkillsControllers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillsControllers : MonoBehaviour
+{
+    //1.
+    //2.
+    //3.
+    //4. Prefabs của skill Hasagi
+    public GameObject hasagiPrefabs;
+    public float shootForce = 10f;
+    public HasagiSkill hasagiSkill;
+
+    void Update()
+    {
+        //4. Sử dụng skill Hasagi
+        if (Input.GetKey(KeyCode.E))
+        {
+            ActivateHasagi();
+        }
+    }
+
+    void ActivateHasagi()
+    {
+        hasagiSkill.ActivateHasagiSkill();
+    }
+}

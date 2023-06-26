7df6a1410b312971bd47eeb8385852f19932b36b
Nguyen Phong Hao <haonphe153346@fpt.edu.vn>
2023-06-26 14:22:03 +0700

    test1

--- Assets/Scripts/SKills/HasagiSkill.cs
+++ Assets/Scripts/SKills/HasagiSkill.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class HasagiSkill : MonoBehaviour
 {
     [SerializeField] private VariableJoystick joystick;
@@ -12,7 +13,33 @@ public class HasagiSkill : MonoBehaviour
     public float hasagi_speed = 10f;
     public bool isCooldown = false;
     private GameObject hasagi;
-    
+    public Image cooldownImage;
+    public TextMeshProUGUI timeText;
+    private float currentCooldownTime = 0f;
+
+    public void Start()
+    {
+        cooldownImage.fillAmount = 0;
+        timeText.gameObject.SetActive(false);
+        cooldownImage.enabled= false;
+
+    }
+
+    public void Update()
+    {
+        if (isCooldown)
+        {
+            currentCooldownTime -= Time.deltaTime;
+            if (currentCooldownTime <= 0)
+            {
+                currentCooldownTime = 0;
+                isCooldown = false;
+                timeText.gameObject.SetActive(false);
+                cooldownImage.enabled= true;
+            }
+            UpdateCooldownUI();
+        }
+    }
 
     public void ActivateHasagiSkill()
     {
@@ -20,13 +47,14 @@ public class HasagiSkill : MonoBehaviour
         {
             return;
         }
+
         var horizontalMoveJoystick = joystick.Horizontal;
         var verticalMoveJoystick = joystick.Vertical;
 
         if (horizontalMoveJoystick == 0 && verticalMoveJoystick == 0)
         {
             var prefab = Resources.Load("HasagiSkills") as GameObject;
-            hasagi = Instantiate(prefab, new Vector3(transform.position.x,transform.position.y,0), Quaternion.identity);
+            hasagi = Instantiate(prefab, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
             Rigidbody2D ballRigidbody = hasagi.GetComponent<Rigidbody2D>();
             ballRigidbody.AddForce(Vector3.right * hasagi_speed, ForceMode2D.Impulse);
         }
@@ -38,18 +66,33 @@ public class HasagiSkill : MonoBehaviour
             Rigidbody2D ballRigidbody = hasagi.GetComponent<Rigidbody2D>();
             ballRigidbody.AddForce(forceDirection * hasagi_speed, ForceMode2D.Impulse);
         }
+
         StartCooldown();
     }
 
     private void StartCooldown()
     {
         isCooldown = true;
-        Invoke("HasagiFinishCooldown", hasagi_coolDown);
+        currentCooldownTime = hasagi_coolDown;
+        timeText.gameObject.SetActive(true);
+        UpdateCooldownUI();
+        cooldownImage.enabled= true;
 
+        Invoke("HasagiFinishCooldown", hasagi_coolDown);
     }
+
+    private void UpdateCooldownUI()
+    {
+        float fillAmount = currentCooldownTime / hasagi_coolDown;
+        cooldownImage.fillAmount = fillAmount;
+        timeText.text = Mathf.RoundToInt(currentCooldownTime).ToString();
+    }
+
     private void HasagiFinishCooldown()
     {
         isCooldown = false;
         Destroy(hasagi);
+        cooldownImage.enabled= false;
+        timeText.gameObject.SetActive(false);
     }
 }
